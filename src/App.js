ad4765f6b39f386ac9ab05cfad2960fe1bdf0418
Yosliana <yoslianaguilar@gmail.com>
2020-07-01 16:03:11 -0400

    Componete Hora

--- src/App.js
+++ src/App.js
@@ -2,22 +2,7 @@ import React from 'react';
 import './img/App.css';
 
 
-
-
 function App() {
-
-  let today = new Date();
-  let year = today.getFullYear();
-  let month = today.getMonth() + 1;
-  month = (month < 10 ? "0" : "") + month;
-  let day  = today.getDate();
-  day = (day < 10 ? "0" : "") + day;
-  let hour = today.getHours();
-   hour = (hour < 10 ? "0" : "") + hour;
-  let  min  = today.getMinutes();
-    min = (min < 10 ? "0" : "") + min;
- // let sec  = today.getSeconds();
-    //sec = (sec < 10 ? "0" : "") + sec;
 
   return (
     <div className="App">
@@ -25,14 +10,15 @@ function App() {
       <div className='fondo'>
         <div id='contenedorLogin'>
           <div id='dia'>
-            <p className='dia'> { year + '-' + month + '-' + day }</p>
-            <p className='hora'>{ hour + ':' + min }</p>
+  <p className='dia'> { year + '-' + month + '-' + day }</p>
+  <p className='hora'>{ hour + ':' + min }</p>
           </div>
           <input id='usuarioLogin' type='text' placeholder='Usuario'></input>
           <input id='contraseñaLogin' type='text' placeholder='Contraseña'></input>
           <button id='iniciar'>Iniciar</button>
         </div>
       </div>
+
     </div>
   );
 }
---
+++ src/ComponentsDataHours.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class ComponentsDataHours extends React.Component{
+  render() {
+    let today = new Date();
+    let year = today.getFullYear();
+    let month = today.getMonth() + 1;
+    month = (month < 10 ? "0" : "") + month;
+    let day  = today.getDate();
+    day = (day < 10 ? "0" : "") + day;
+    let hour = today.getHours();
+    hour = (hour < 10 ? "0" : "") + hour;
+    let  min  = today.getMinutes();
+    min = (min < 10 ? "0" : "") + min;
+    return(
+      <div>
+       { year + '-' + month + '-' + day }
+       { hour + ':' + min }
+      </div>
+    )
+  }
+}
+export default ComponentsDataHours;
