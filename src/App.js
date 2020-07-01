81a6bd71e79a8580e8fbbc111fff196e806844a5
Yosliana <yoslianaguilar@gmail.com>
2020-07-01 12:12:36 -0400

    Fecha y Hora

--- src/App.js
+++ src/App.js
@@ -3,14 +3,28 @@ import './img/App.css';
 
 
 function App() {
+
+  let today = new Date();
+  let year = today.getFullYear();
+  let month = today.getMonth() + 1;
+  month = (month < 10 ? "0" : "") + month;
+  let day  = today.getDate();
+  day = (day < 10 ? "0" : "") + day;
+  let hour = today.getHours();
+   hour = (hour < 10 ? "0" : "") + hour;
+  let  min  = today.getMinutes();
+    min = (min < 10 ? "0" : "") + min;
+ // let sec  = today.getSeconds();
+    //sec = (sec < 10 ? "0" : "") + sec;
+
   return (
     <div className="App">
 
       <div className='fondo'>
         <div id='contenedorLogin'>
           <div id='dia'>
-            <p className='dia'>28-06-2020</p>
-            <p className='hora'>09:00</p>
+  <p className='dia'> { year + '-' + month + '-' + day }</p>
+  <p className='hora'>{ hour + ':' + min }</p>
           </div>
           <input id='usuarioLogin' type='text' placeholder='Usuario'></input>
           <input id='contraseñaLogin' type='text' placeholder='Contraseña'></input>
